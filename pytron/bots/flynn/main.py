607c70c286b8f84c16314b4945562354a420a137
Jordi Bagot <jbagot@kavehome.com>
2022-04-17 17:40:38 +0200

    Last status with calculus

--- pytron/bots/flynn/main.py
+++ pytron/bots/flynn/main.py
@@ -9,16 +9,16 @@ class PlayerBot(Bot):
         self.current = 0
         self.margin = 3
         self.margin_iteration = 1
-        self.action = "forward"
+        self.state = "forward"
         self.goal = self.margin
-        # self.
+        self.turning = False
 
 
     def close_to_edge(self, margin=1) -> bool:
         x, y = self.board.bots_path[self.id][-1]
-        return ((x <= margin) or 
+        return ((x <= margin) or
                 (y <= margin) or
-                (x >= self.board_column_size-margin) or 
+                (x >= self.board_column_size-margin) or
                 (y >= self.board_row_size-margin))
 
 
@@ -28,24 +28,32 @@ class PlayerBot(Bot):
             if (x0, y0) in self.board.bots_path[self. id]:
                 continue
             if abs(x-x0) <= margin or abs(y-y0) <= margin:
-                return True        
+                return True
         return False
 
 
     def get_action(self, board):
         self.board = board
 
-        # start forward, turn right, start reverse, turn left
+        # Check when spiral is going big if it collides with something
+        # breakpoint()
+        if self.turning:
+            self.turning = False
+            return Action.Right
 
-        # if self.warning_collision():
-        #     # state = self.next_state()
-        #     self.action = "reverse"
+        if self.state == "forward" and (self.close_to_bot() or self.close_to_edge()):
+            self.state = "reverse"
+            self.current = 0
+            self.n_cycles = 0
+            self.turning = True
+            return Action.Right
 
-        if self.action == "reverse":
-            # breakpoint()
-            return self.reverse()
-        else:
-            return self.forward()
+        return getattr(self, self.state)()
+
+        # if self.state == "reverse":
+        #     return self.reverse()
+        # else:
+        #     return self.forward()
 
     def forward(self):
         # right, right, margin, right, margin, right, margin*2, right, margin*2,
@@ -68,7 +76,23 @@ class PlayerBot(Bot):
         return Action.Forward
 
     def reverse(self):
-        pass
+        print(f"reverse {self.n_cycles=} {self.current=} {self.goal=}")
+
+        if self.current == self.goal-1:
+            self.current = 0
+            if self.n_cycles:
+                self.n_cycles = False
+                self.margin_iteration -= 1
+                self.goal = self.margin_iteration * self.margin
+            else:
+                self.n_cycles = True
+
+            print("LEFT")
+            return Action.Left
+
+        self.current += 1
+        print("FORWARD")
+        return Action.Forward
 
 
     def warning_collision(self):
