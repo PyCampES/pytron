301c1443d0a33701a6b30883acbf2775ff93f2b0
Jordi Bagot <jbagot@kavehome.com>
2022-04-17 16:02:47 +0200

    Add new flynn bot

---
+++ pytron/bots/flynn/main.py
@@ -0,0 +1,94 @@
+from pytron.bot import Bot, Action
+
+
+class PlayerBot(Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.n_cycles = 0
+        self.n_forwards = 0
+        self.current = 0
+        self.margin = 3
+        self.margin_iteration = 1
+        self.action = "forward"
+        self.goal = self.margin
+        self.state = ["f", "t", "r", "t"]
+
+
+    def get_action(self, board):
+
+        # start forward, turn right, start reverse, turn left
+
+
+        if self.action == "reverse":
+            # breakpoint()
+            return self.reverse()
+        else:
+            return self.forward()
+
+    def forward(self):
+        # right, right, margin, right, margin, right, margin*2, right, margin*2,
+        print(f"forward {self.n_cycles=} {self.current=} {self.goal=}")
+
+        if self.current == self.goal:
+            self.current = 0
+            if self.n_cycles:
+                self.n_cycles = False
+                self.margin_iteration += 1
+                self.goal = self.margin_iteration * self.margin
+            else:
+                self.n_cycles = True
+
+            print("RIGHT")
+            return Action.Right
+
+        self.current += 1
+        print("FORWARD")
+        return Action.Forward
+
+    def reverse(self):
+        pass
+
+
+    # def forward(self):
+
+
+    #     if self.n_forwards == 9:
+    #         # Return to same n_forwards
+    #         self.n_forwards -= self.margin
+    #         self.margin -= 1
+    #         self.n_cycles = 1
+    #         print("RIGHT TURN")
+    #         self.action = "reverse"
+    #         return Action.Right
+    #     print(f"forward {self.n_cycles=} {self.n_forwards=} {self.n_current_forwards=}")
+
+    #     if self.n_current_forwards == self.n_forwards:
+    #         if self.n_cycles == 1:
+    #             self.n_cycles = 0
+    #             self.n_forwards += self.margin
+    #         else:
+    #             self.n_cycles += 1
+    #         self.n_current_forwards = 0
+    #         print("RIGHT")
+    #         return Action.Right
+    #     self.n_current_forwards += 1
+    #     print("FORWARD")
+    #     return Action.Forward
+
+    # def reverse(self):
+    #     print(f"reverse {self.n_cycles=} {self.n_forwards=} {self.n_current_forwards=} {self.margin=}")
+    #     if self.n_forwards == 0:
+    #         self.is_reverse = False
+
+    #     if self.n_current_forwards == self.n_forwards-1:
+    #         if self.n_cycles == 1:
+    #             self.n_cycles = 0
+    #             self.n_forwards -= self.margin
+    #         else:
+    #             self.n_cycles += 1
+    #         self.n_current_forwards = 0
+    #         print("LEFT")
+    #         return Action.Left
+    #     self.n_current_forwards += 1
+    #     print("FORWARD")
+    #     return Action.Forward
